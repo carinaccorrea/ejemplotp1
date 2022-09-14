fcef9845ceb97c837626d16138a8ad2db604655e
carinaccorrea <carinaccorrea@gmail.com>
2022-09-14 15:19:46 -0300

    Update usuarioController.js
    se modificaron metodos

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,14 +1,17 @@
 module.exports={
 
 listar:async (req,res)=>{
+    console.log('ejecutando listar usuarios en consola.');
     res.json({ message: 'SE LISTAN USUARIOS' });
     
 },
 crear:async (req,res)=>{
+    console.log('ejecutando crear usuario en consola.');
     res.json({ message: 'SE CREA 1 USUARIO' });
     
 },
 listarInfo:async (req,res)=>{
+    console.log('ejecutando listar un usuario en consola.');
     res.json({ message: 'SE LISTA INFO DEL USUARIO' });
 },
 
